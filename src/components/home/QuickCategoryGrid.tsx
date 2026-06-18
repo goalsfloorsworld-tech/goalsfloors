ce5a4456029fa30ae091989dd24123c65959d3df
Neyab <developer.neyabofficial@gmail.com>
2026-06-18 15:32:01 +0530

    refe

--- src/components/home/QuickCategoryGrid.tsx
+++ src/components/home/QuickCategoryGrid.tsx
@@ -19,19 +19,19 @@ import {
 } from "lucide-react";
 
 const categories = [
-  { id: 1, name: "Wall Panels", icon: Columns2, iconImage: "/panels.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772477058/Wpc_Fluted_Panel_In_Gurgaon.png", bgColor: "bg-[#e8f3f1]" },
-  { id: 2, name: "Upfit Panels", icon: Columns3, iconImage: "/upfit.png", iconSize: "w-8 h-8", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1774525633/goals-floors-upfit-panels-m7Vw49PE2qSaDNaG.jpg", bgColor: "bg-[#f4efe8]" },
-  { id: 3, name: "Tokyo Moulding", icon: AlignLeft, iconImage: "/moulding.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1774970794/charcoal-gray_paneled_wall..jpg", bgColor: "bg-[#e8f1f5]" },
-  { id: 4, name: "Cobra PU Stone", icon: Mountain, iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772477246/Pu_Stone_Colors.jpg", bgColor: "bg-[#f5eef4]" },
-  { id: 5, name: "Exterior Louvers", icon: AlignJustify, iconImage: "/louvers.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775818177/exterior_louvaring.jpg", bgColor: "bg-[#edf5e8]" },
-  { id: 6, name: "Artificial Grass", icon: Leaf, iconImage: "/grass.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772476444/artificial_grass_supplier_in_india.jpg", bgColor: "bg-[#f5ece8]" },
-  { id: 7, name: "WPC Decking", icon: Sun, iconImage: "/decking.png", iconSize: "w-8 h-8", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772478659/premium_wpc_decking_supplier_in_gurgaon.jpg", bgColor: "bg-[#e8ebf5]" },
-  { id: 8, name: "SPC Flooring", icon: LayoutGrid, iconImage:"/spc.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1774612492/screenshot-2024-11-05-074302-YKb68R7lNDueGKEV.jpg", bgColor: "bg-[#f5e8e8]" },
-  { id: 9, name: "Laminate Flooring", icon: Layers, iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775544853/oak_color_laminate_flooring_installed_images.png", bgColor: "bg-[#e8f5ed]" },
-  { id: 10, name: "Herringbone", icon: Grid2X2,iconImage:"/heringbone.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775411703/Luxury_Penthouse_Living_Room_with_Light_Oak_Herringbone_Flooring_Gurugram.jpg", bgColor: "bg-[#f5f2e8]" },
-  { id: 11, name: "Hybrid Flooring", icon: Box, iconImage:"/hybrid.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772477090/Ocean_Waterproof_Laminate_FLooring.jpg", bgColor: "bg-[#e8eef5]" },
-  { id: 12, name: "Baffle Ceiling", icon: GripHorizontal, iconImage:"/baffle.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775466342/screenshot-2024-10-27-180236-1-AQEyyylJkGUBqyED.webp", bgColor: "bg-[#f2e8f5]" },
-  { id: 13, name: "Timber Tubes", icon: Pause, iconImage:"/timber.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772478739/Timber_Tube.png", bgColor: "bg-[#f5e8ed]" },
+  { id: 1, name: "Wall Panels", href: "/products/wall-panels", icon: Columns2, iconImage: "/panels.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772477058/Wpc_Fluted_Panel_In_Gurgaon.png", bgColor: "bg-[#e8f3f1]" },
+  { id: 2, name: "Upfit Panels", href: "/products/upfit-panels", icon: Columns3, iconImage: "/upfit.png", iconSize: "w-8 h-8", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1774525633/goals-floors-upfit-panels-m7Vw49PE2qSaDNaG.jpg", bgColor: "bg-[#f4efe8]" },
+  { id: 3, name: "Tokyo Moulding", href: "/products/tokyo-charcoal-moulding", icon: AlignLeft, iconImage: "/moulding.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1774970794/charcoal-gray_paneled_wall..jpg", bgColor: "bg-[#e8f1f5]" },
+  { id: 4, name: "Cobra PU Stone", href: "/products/cobra-pu-stone", icon: Mountain, iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772477246/Pu_Stone_Colors.jpg", bgColor: "bg-[#f5eef4]" },
+  { id: 5, name: "Exterior Louvers", href: "/products/wpc-exterior-louvers", icon: AlignJustify, iconImage: "/louvers.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775818177/exterior_louvaring.jpg", bgColor: "bg-[#edf5e8]" },
+  { id: 6, name: "Artificial Grass", href: "/products/artificial-grass", icon: Leaf, iconImage: "/grass.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772476444/artificial_grass_supplier_in_india.jpg", bgColor: "bg-[#f5ece8]" },
+  { id: 7, name: "WPC Decking", href: "/products/wpc-decking", icon: Sun, iconImage: "/decking.png", iconSize: "w-8 h-8", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772478659/premium_wpc_decking_supplier_in_gurgaon.jpg", bgColor: "bg-[#e8ebf5]" },
+  { id: 8, name: "SPC Flooring", href: "/products/spc-flooring", icon: LayoutGrid, iconImage:"/spc.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1774612492/screenshot-2024-11-05-074302-YKb68R7lNDueGKEV.jpg", bgColor: "bg-[#f5e8e8]" },
+  { id: 9, name: "Laminate Flooring", href: "/products/laminate-flooring", icon: Layers, iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775544853/oak_color_laminate_flooring_installed_images.png", bgColor: "bg-[#e8f5ed]" },
+  { id: 10, name: "Herringbone", href: "/products/herringbone-laminate-flooring", icon: Grid2X2,iconImage:"/heringbone.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775411703/Luxury_Penthouse_Living_Room_with_Light_Oak_Herringbone_Flooring_Gurugram.jpg", bgColor: "bg-[#f5f2e8]" },
+  { id: 11, name: "Hybrid Flooring", href: "/products/hybrid-laminate-flooring", icon: Box, iconImage:"/hybrid.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772477090/Ocean_Waterproof_Laminate_FLooring.jpg", bgColor: "bg-[#e8eef5]" },
+  { id: 12, name: "Baffle Ceiling", href: "/products/wpc-baffle-ceiling", icon: GripHorizontal, iconImage:"/baffle.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1775466342/screenshot-2024-10-27-180236-1-AQEyyylJkGUBqyED.webp", bgColor: "bg-[#f2e8f5]" },
+  { id: 13, name: "Timber Tubes", href: "/products/wpc-timber-tubes", icon: Pause, iconImage:"/timber.png", iconSize: "w-7 h-7", image: "https://res.cloudinary.com/dcezlxt8r/image/upload/f_auto,q_auto/v1772478739/Timber_Tube.png", bgColor: "bg-[#f5e8ed]" },
 ];
 
 export default function QuickCategoryGrid() {
@@ -70,7 +70,7 @@ export default function QuickCategoryGrid() {
             {categories.map((category) => (
               <Link
                 key={category.id}
-                href="#"
+                href={category.href || "#"}
                 className="flex flex-col items-center group cursor-pointer w-1/4 sm:w-[100px] md:w-[120px]"
               >
                 <div
@@ -104,7 +104,7 @@ export default function QuickCategoryGrid() {
             {categories.map((category) => (
               <Link
                 key={category.id}
-                href="#"
+                href={category.href || "#"}
                 className="flex flex-col items-center flex-shrink-0 gap-1.5 group cursor-pointer"
               >
                 <div className={`w-10 h-10 rounded-full ${category.bgColor} dark:bg-slate-800 flex items-center justify-center transition-transform group-hover:scale-110 shadow-sm overflow-hidden`}>
